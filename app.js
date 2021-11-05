602e10d43b67ca412c3afb307ed9ee90602e3feb
karanh <karan.handa.fox@gmail.com>
2021-11-05 11:11:46 +0530

    Added database and mongoose

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var password = 'binkbonk123'
+mongoose.connect('mongodb+srv://karanh:' + password + '@test-db.ymyhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
